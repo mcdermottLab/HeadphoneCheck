a624f4be16fd4560242dc6a7332b850e11edc487
Ray Gonzalez <raysgon@yahoo.com>
2017-03-29 22:22:06 -0400

    replaced single quotes with double; removed an alert

--- HeadphoneCheck.js
+++ HeadphoneCheck.js
@@ -6,10 +6,10 @@
 
   /*** PRIVATE CONFIGURATION VARIABLES ***/
   var doShuffleTrials = true;
-  var validColor = 'black';
-  var warningColor = 'red';
-  var requirePlayback = false;
-  var defaultAudioType = 'audio/mpeg';
+  var validColor = "black";
+  var warningColor = "red";
+  var requirePlayback = true;
+  var defaultAudioType = "audio/mpeg";
 
   var totalCorrect = 0;
   var stimMap = [];
@@ -28,16 +28,16 @@
     stimMap = shuffle(data.stim); //shuffles the array
   }
 
-  // TODO: fix this, this doesn't work
+  // TODO: fix this, this doesn"t work
   function parseAudioType(stimID) {
-    console.log('TYPE: ' +stimID);
+    console.log("TYPE: " +stimID);
     console.log(stimMap);
     var typeStr = stimMap[stimID];
-    console.log('TYPE: '+typeStr);
+    console.log("TYPE: "+typeStr);
     if (typeStr === undefined) {
       typeStr = defaultAudioType;
     }
-    console.log('TYPE: '+typeStr);
+    console.log("TYPE: "+typeStr);
     return typeStr;
   }
 
@@ -51,28 +51,28 @@
   function playStim(stimID) {
     var stimFile = "audio" + stimID;
     // set onended callback
-    $('#' + stimFile).on("ended", function() {
+    $("#" + stimFile).on("ended", function() {
       // reset playback state
       st_isPlaying = false;
 
       // activate responses
       if (requirePlayback) {
-        $('#radioButtons' + stimID).css('pointer-events', 'auto');
+        $("#radioButtons" + stimID).css("pointer-events", "auto");
       }
     });
 
-    $('#' + stimFile).get(0).play();
+    $("#" + stimFile).get(0).play();
     st_isPlaying = true;
   }
 
   function disableClick(buttonID) {
-    $('#' + buttonID).prop('disabled', true);
+    $("#" + buttonID).prop("disabled", true);
   }
 
   function checkCanContinue() {
     // Check that each question has a response, if not, highlight what is needed
-    // TODO: This is HACKY and probably isn't the best idea
-    numResponses = $('.ui-buttonset-vertical>label>input[type=radio]:checked').length;
+    // TODO: This is HACKY and probably isn"t the best idea
+    numResponses = $(".ui-buttonset-vertical>label>input[type=radio]:checked").length;
     return numResponses >= HeadphoneCheck.examplesPerPage; // easy for user to circumvent check
   }
 
@@ -80,19 +80,19 @@
     // toggle the response warnings
 
     // get parent div of anything checked, should only be 1 radio button per div
-    var checked = $('.ui-buttonset-vertical>label>input[type=radio]:checked').parent().parent();
+    var checked = $(".ui-buttonset-vertical>label>input[type=radio]:checked").parent().parent();
 
     // get parent divs of anything unchecked, can be as many as # of responses
-    var unchecked = $('.ui-buttonset-vertical>label>input[type=radio]').not(':checked').parent().parent();
+    var unchecked = $(".ui-buttonset-vertical>label>input[type=radio]").not(":checked").parent().parent();
 
     // get all top level divs (i.e., trial containers) without any responses
     var uncheckedTrials = $(unchecked).not(checked).parent();
 
     // hide warning on completed trials
-    $(checked).parent().css('border', '5px solid ' + validColor);
+    $(checked).parent().css("border", "5px solid " + validColor);
 
     // show warning on empty trials
-    $(uncheckedTrials).css('border', '5px solid ' + warningColor);
+    $(uncheckedTrials).css("border", "5px solid " + warningColor);
   }
 
   function shuffle(array) {
@@ -117,25 +117,25 @@
   function renderOneTrialHTML(stimDiv, stimID, stimFile) {
     var divID = "stim" + stimID;
     console.log(divID);
-    $('<div/>', {id: divID, class: 'trialDiv'}).appendTo(('#' + stimDiv));
+    $("<div/>", {id: divID, class: "trialDiv"}).appendTo(("#" + stimDiv));
 
     //add in the audio source
-    $('<audio/>', {
-        id: 'audio' + stimID,
-        // type: 'audio/mpeg', // TODO: Factor this out, should be user defined
+    $("<audio/>", {
+        id: "audio" + stimID,
+        // type: "audio/mpeg", // TODO: Factor this out, should be user defined
         // type: parseAudioType(stimID),
         src: stimFile
-      }).appendTo($('#' + divID));
+      }).appendTo($("#" + divID));
 
     if (HeadphoneCheck.debug) {
-      $('<div/>', {
-          text: 'Trial ID: ' + stimID
-      }).appendTo($('#' + divID));
+      $("<div/>", {
+          text: "Trial ID: " + stimID
+      }).appendTo($("#" + divID));
     }
 
     //add in the button for playing the sound
-    $('<button/>', {
-      id: 'b' + stimID,
+    $("<button/>", {
+      id: "b" + stimID,
       disabled: false,
       click: function () {
         if (!st_isPlaying){
@@ -144,15 +144,15 @@
         }
         else {}
       },
-      text: 'Play'
-    }).appendTo($('#' + divID));
+      text: "Play"
+    }).appendTo($("#" + divID));
 
     //add in the radio buttons for selecting which sound was softest
-    $('<div/>', {
-      id: 'radioButtons'+stimID,
-      class: 'ui-buttonset-vertical',
-      // width: '30%'
-    }).appendTo($('#' + divID));
+    $("<div/>", {
+      id: "radioButtons"+stimID,
+      class: "ui-buttonset-vertical",
+      // width: "30%"
+    }).appendTo($("#" + divID));
 
     //give the label info for the buttons
     var radioButtonInfo = [
@@ -163,16 +163,16 @@
 
     $.each(radioButtonInfo, function() {
       $("#radioButtons" + stimID).append(
-        $('<label/>', {
-          for: 'radio' + this.id,
-          class: 'radio-label',
+        $("<label/>", {
+          for: "radio" + this.id,
+          class: "radio-label",
           text: this.name
         }).prepend(
-            $('<input/>', {
-                type: 'radio',
-                id: 'radio' + this.id,
-                name: 'radio-resp' + stimID,
-                class: 'radio-responses',
+            $("<input/>", {
+                type: "radio",
+                id: "radio" + this.id,
+                name: "radio-resp" + stimID,
+                class: "radio-responses",
                 value: this.id
             })
           )
@@ -181,7 +181,7 @@
   }
 
   function teardownHTMLPage(pageNum) {
-    $('#container-exp').empty();
+    $("#container-exp").empty();
   }
 
   //PUBLIC FUNCTIONS
@@ -200,7 +200,6 @@
             }
             console.log(stimMap);
             lastPage = Math.ceil(stimMap.length / HeadphoneCheck.examplesPerPage); //get last page
-            alert(lastPage)
         }
     });
   };
@@ -213,58 +212,58 @@
     }
     console.log(curStimuli);
 
-    $('<div/>', {
+    $("<div/>", {
       id: "instruct",
       class: "warning",
       html: "When you hit <b>Play</b>, you will hear three sounds separated by silences."
-    }).appendTo($('#container-exp'));
+    }).appendTo($("#container-exp"));
 
 
-    $('<div/>', {
+    $("<div/>", {
       class: "warning",
       text: "Simply judge WHICH SOUND WAS SOFTEST(quietest)-- 1, 2, or 3?"
-    }).appendTo($('#container-exp'));
+    }).appendTo($("#container-exp"));
 
-    $('<div/>', {
+    $("<div/>", {
       class: "warning",
       text: "Test sounds can only be played once!"
-    }).appendTo($('#container-exp'));
+    }).appendTo($("#container-exp"));
 
     // add in a group for each item in stimulus
     $.each(curStimuli, function () {
-      renderOneTrialHTML('container-exp', this.id, this.stimFile);
+      renderOneTrialHTML("container-exp", this.id, this.stimFile);
     });
 
     if (requirePlayback) {
       // no response until the sound is played
-      $('.ui-buttonset-vertical').css('pointer-events', 'none');
+      $(".ui-buttonset-vertical").css("pointer-events", "none");
     }
 
     // Add button to continue
-    $('<button/>', {
+    $("<button/>", {
       class: "warning",
       // type: "button",
       text: "Continue",
       click: function () {
         var canContinue = checkCanContinue();
         for (stimID = 0; stimID < HeadphoneCheck.examplesPerPage; stimID++) {
-          updateCorrect(curStimuli[stimID], $('input[name=radio-resp' + stimID + ']:checked').val());
+          updateCorrect(curStimuli[stimID], $("input[name=radio-resp" + stimID + "]:checked").val());
         }
         if (pageNum == lastPage - 1) { // TODO: -1 for indexing; make indexing consistent
           teardownHTMLPage(pageNum);
           checkPassFail();
-          alert('done with headphone check')
+          alert("done with headphone check")
         }
         else if (canContinue) { // Advance the page
           teardownHTMLPage(pageNum);
           pageNum++;
           HeadphoneCheck.renderHTMLPage(pageNum);
         }
-        else { // need responses, don't advance page, show warnings
+        else { // need responses, don"t advance page, show warnings
           renderResponseWarnings();
         }
       }
-    }).appendTo($('#container-exp'));
+    }).appendTo($("#container-exp"));
   };
 }( window.HeadphoneCheck = window.HeadphoneCheck || {}, jQuery));
 
@@ -274,7 +273,7 @@ $(document).ready(function() {
   var pageNum = 0;
   var continueVal = HeadphoneCheck.renderHTMLPage(pageNum);
   if (continueVal) {
-    alert('continuing on!');
+    alert("continuing on!");
   }
 });
 
