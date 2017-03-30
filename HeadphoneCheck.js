0f8140059007db30e937e9d84223b8618958074d
Ray Gonzalez <raysgon@yahoo.com>
2017-03-29 20:18:15 -0400

    Added styling and logic for page advancing

--- HeadphoneCheck.js
+++ HeadphoneCheck.js
@@ -1,7 +1,12 @@
 (function(HeadphoneCheck, $, undefined) {
 
+  /*** PUBLIC CONFIGURATION VARIABLES ***/
   HeadphoneCheck.examplesPerPage = 3;
   HeadphoneCheck.debug = true;
+
+  /*** PRIVATE CONFIGURATION VARIABLES ***/
+  var validColor = 'black';
+  var warningColor = 'red';
 
   var totalCorrect = 0;
   var stimMap = [];
@@ -32,6 +37,32 @@
 
   function disableClick(buttonID) {
     $('#' + buttonID).prop('disabled', true);
+  }
+
+  function checkCanContinue() {
+    // Check that each question has a response, if not, highlight what is needed
+    // TODO: This is HACKY and probably isn't the best idea
+    numResponses = $('.ui-buttonset-vertical>label>input[type=radio]:checked').length;
+    return numResponses >= HeadphoneCheck.examplesPerPage; // easy for user to circumvent check
+  }
+
+  function renderResponseWarnings() {
+    // toggle the response warnings
+
+    // get parent div of anything checked, should only be 1 radio button per div
+    var checked = $('.ui-buttonset-vertical>label>input[type=radio]:checked').parent().parent();
+
+    // get parent divs of anything unchecked, can be as many as # of responses
+    var unchecked = $('.ui-buttonset-vertical>label>input[type=radio]').not(':checked').parent().parent();
+
+    // get all top level divs (i.e., trial containers) without any responses
+    var uncheckedTrials = $(unchecked).not(checked).parent();
+
+    // hide warning on completed trials
+    $(checked).parent().css('border', '5px solid ' + validColor);
+
+    // show warning on empty trials
+    $(uncheckedTrials).css('border', '5px solid ' + warningColor);
   }
 
   function shuffle(array) {
@@ -80,7 +111,7 @@
     $('<div/>', {
       id: 'radioButtons'+stimID,
       class: 'ui-buttonset-vertical',
-      width: '30%'
+      // width: '30%'
     }).appendTo($('#' + divID));
 
     //give the label info for the buttons
@@ -88,14 +119,15 @@
 
     $.each(radioButtonInfo, function() {
       $("#radioButtons" + stimID).append(
-        $('<label />', {
+        $('<label/>', {
           for: 'radio' + this.id,
           class: 'radio-label',
           text: this.name
-        }).prepend($('<input />', {
+        }).prepend($('<input/>', {
                     type: 'radio',
                     id: 'radio' + this.id,
                     name: 'radio-resp' + stimID,
+                    class: 'radio-responses',
                     value: this.id
                    }))
         );
@@ -116,13 +148,12 @@
             }
             shuffleTrials(data);
             console.log(stimMap);
-            lastPage = Math.ceil(stimMap.length/HeadphoneCheck.examplesPerPage); //get last page
+            lastPage = Math.ceil(stimMap.length / HeadphoneCheck.examplesPerPage); //get last page
         }
     });
   };
 
   HeadphoneCheck.renderHTMLPage = function(pageNum) {
-
     //get the stimuli to display on this page
     var curStimuli = [];
     for (i = 0; i < HeadphoneCheck.examplesPerPage; i++) {
@@ -130,19 +161,19 @@
     }
     console.log(curStimuli);
 
-    $('<div />', {
+    $('<div/>', {
       id: "instruct",
       class: "warning",
       html: "When you hit <b>Play</b>, you will hear three sounds separated by silences."
     }).appendTo($('#container-exp'));
 
 
-    $('<div />', {
+    $('<div/>', {
       class: "warning",
       text: "Simply judge WHICH SOUND WAS SOFTEST(quietest)-- 1, 2, or 3?"
     }).appendTo($('#container-exp'));
 
-    $('<div />', {
+    $('<div/>', {
       class: "warning",
       text: "Test sounds can only be played once!"
     }).appendTo($('#container-exp'));
@@ -152,20 +183,25 @@
       renderOneTrialHTML('container-exp', this.id, this.stimFile);
     });
 
-    $('<div />', {
+    // Add button to continue
+    $('<button/>', {
       class: "warning",
-      type: "button",
+      // type: "button",
       text: "Continue",
       click: function () {
+        var canContinue = checkCanContinue();
         for (stimID = 0; stimID < HeadphoneCheck.examplesPerPage; stimID++) {
           updateCorrect(curStimuli[stimID], $('input[name=radio-resp' + stimID + ']:checked').val());
         }
         if (pageNum == lastPage) {
           checkPassFail();
         }
-        else {
+        else if (canContinue) { // Advance the page
           pageNum++;
           HeadphoneCheck.renderHTMLPage(pageNum);
+        }
+        else { // need responses, don't advance page, show warnings
+          renderResponseWarnings();
         }
       }
     }).appendTo($('#container-exp'));
